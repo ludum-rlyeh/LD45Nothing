b2fc6a42f5334c55cc2fd3a8c16435ca83ee33f7
Avzgui <contact.avzgui@gmail.com>
2019-10-05 22:42:13 +0200

    boids algorithm for circles

--- scripts/Utils.gd
+++ scripts/Utils.gd
@@ -7,3 +7,17 @@ func remove_duplicates(var array):
 		if not n_array.has(val):
 			n_array.append(val)
 	return n_array
+
+# test and teleport boid if out of viewport
+func out_of_viewport(var boid):
+	var viewport_size = get_viewport().get_size()
+	
+	if boid.position.x < -viewport_size.x/4.0 :
+		boid.position.x = viewport_size.x + viewport_size.x/4.0
+	elif boid.position.x > viewport_size.x + viewport_size.x/4.0 :
+		boid.position.x = -viewport_size.x/4.0
+	
+	if boid.position.y < -viewport_size.y / 4.0 :
+		boid.position.y = viewport_size.y + viewport_size.y / 4.0
+	elif boid.position.y > viewport_size.y + viewport_size.y / 4.0:
+		boid.position.y = - viewport_size.y / 4.0
--- scripts/especes/NodePoint.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	pass
-
-func build(var points):
-	$Polygon2D.build(points)
