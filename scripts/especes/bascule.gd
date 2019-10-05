39cf94eda69da64500e95bc81e3836a31bb5ebcd
chtimy <jeremy.riffet@gmail.com>
2019-10-05 10:38:24 +0200

    Ajout bascule (pas fini)

--- scripts/especes/bascule.gd
+++ scripts/especes/bascule.gd
@@ -1,13 +1,46 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var MIN_ANGLE = 20
+var MAX_ANGLE = 250
+
+var origin : Vector2
+var vec_line : Vector2
+var length
+var cumulate_angle = 0
+var total_angle
+var SPEED_ANGLE = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	build([50,0])
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass # Replace with function body.
+	
+func _process(delta):
+	move(delta)
+
+func build(shape):
+	origin = shape[0]
+	vec_line = shape[1] - origin
+	length = vec_line.length()
+	total_angle = new_angle()
+	
+	var points = [origin + Vector2(0, 10), origin - Vector2(0, 10), origin + vec_line + Vector2(0, -10), origin + vec_line + Vector2(0, 10)]
+#	self.set_polygon(points)
+	
+func move(delta):
+	var angle = total_angle * delta
+	self.rotate(min(angle, abs(total_angle - cumulate_angle)))
+	cumulate_angle += angle
+	
+	
+	if (cumulate_angle > total_angle):
+		cumulate_angle = 0
+		total_angle = new_angle()
+		
+func random_angle_between(minVal, maxVal):
+	rand_range(minVal , maxVal)
+	
+func new_angle():
+	return random_angle_between(MIN_ANGLE, MAX_ANGLE)
