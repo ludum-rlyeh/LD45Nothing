9aaa0bf02ed25ba786c13965fdeb0dcbe948b58c
chtimy <jeremy.riffet@gmail.com>
2019-10-06 18:36:55 +0200

    blur effect

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,20 +1,51 @@
-extends Node2D
+extends Control
 
-var ABuffer = ViewportTexture.new()
+var canvas
+var viewport
+
+var bg
+
+#var viewport_text0
+#var viewport_text1
+#var viewport_text2
 
 func _ready():
-	randomize()
-	$Canvas.connect("_new_boid_sig", self, "_on_new_boid")
+	viewport = $ViewportContainer/Viewport
+	canvas = $Canvas
 	
-	var viewport = get_viewport()
-	viewport.render_target_clear_mode = Viewport.CLEAR_MODE_NEVER
+	randomize()
+	canvas.connect("_new_boid_sig", self, "_on_new_boid")
+	
+	bg = ImageTexture.new()
+	bg.load("res://assets/BG.jpg")
+	
+	$ViewportContainer.material.set_shader_param("BGTexture", bg)
+#	$ViewportContainer.material.set_shader_param("ViewportTexture", render_text)
+
+#	viewport_text2 = viewport.get_texture();
+#	viewport_text0 = ImageTexture.new()
+#	viewport_text0.create_from_image(viewport.get_texture().get_data())
+#	viewport_text1 = ImageTexture.new()
+#	viewport_text1.create_from_image(viewport.get_texture().get_data())
+	
+	
 
 func _on_new_boid(var boid_type, var line2d):
 	call_deferred("add_boid", boid_type, line2d)
 	
+#
+#func _process(delta):
+#	$ViewportContainer.material.set_shader_param("ViewportTexture0", viewport_text0)
+#	$ViewportContainer.material.set_shader_param("ViewportTexture1", viewport_text1)
+#	$ViewportContainer.material.set_shader_param("ViewportTexture2", viewport_text2)
+#
+#	viewport_text0.set_data(viewport_text1.get_data())
+#	viewport_text1.set_data(viewport_text2.get_data())
+	
 
 func add_boid(var boid_type, var line2d):
-	
+#	var viewport = $Viewport
+#	viewport.render_target_clear_mode = Viewport.CLEAR_MODE_NEVER
 	var boid = null
 	print_debug(boid_type)
 	
@@ -34,6 +65,6 @@ func add_boid(var boid_type, var line2d):
 		
 	if boid != null :
 		boid.build(line2d.points)
-		add_child(boid)
+		viewport.add_child(boid)
 	
 	line2d.queue_free()
